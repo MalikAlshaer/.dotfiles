d1e487a79545d1f52447e1df9848dd5520146b39
Malik Alshaer <malik.h.alshaer@gmail.com>
2026-05-01 16:25:53 +0300

    Delete nvim/lsp directory

--- nvim/lsp/jedi_language_server.lua
+++
@@ -1,7 +0,0 @@
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-vim.lsp.config("jedi_language_server", {
-  capabilities = capabilities,
-  cmd = { 'jedi-language-server' },
-  filetypes = { 'python' },
-})
