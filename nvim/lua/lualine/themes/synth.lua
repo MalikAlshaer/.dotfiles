a6e5f07bf9d20029e5aa7007e4c6c2e02a5c3dca
Malik Alshaer <malik.h.alshaer@gmail.com>
2026-05-02 16:06:17 +0300

    idk

--- nvim/lua/lualine/themes/synth.lua
+++ nvim/lua/lualine/themes/synth.lua
@@ -1,14 +1,14 @@
 local colors = {
-  bg       = "#1B2B34",
-  fg       = "#CDD3DE",
+  bg       = '#1B2B34',
+  fg       = '#CDD3DE',
 
-  blue     = "#6699CC",
-  orange   = "FA8A55",
-  red      = "#EC5F67",
-  green    = "#20BD9d",
+  blue     = '#6699CC',
+  orange   = '#FA8A55',
+  red      = '#EC5F67',
+  green    = '#20BD9d',
 
-  gray     = "#40403C",
-  lightgray= "#767676",
+  gray     = '#40403C',
+  lightgray= '#767676',
 }
 
 local synth = {}
@@ -50,55 +50,3 @@ synth.inactive = {
 }
 
 return synth
-
--- local colors = {
---   bg       = "#1a2022",
---   fg       = "#dadada",
---   gray     = "#454545",
---   lightgray= "#585858",
-
---   blue1    = "#45adc9",
---   blue2    = "#038cfd",
---   cyan     = "#04c0fd",
---   purple   = "#3d3bfc",
---   orange   = "#f1ba86",
---   white    = "#ffffff",
--- }
-
--- return {
---   normal = {
---     a = { fg = colors.bg, bg = colors.blue2, gui = "bold" },
---     b = { fg = colors.fg, bg = colors.gray },
---     c = { fg = colors.fg, bg = colors.bg },
---   },
-
---   insert = {
---     a = { fg = colors.bg, bg = colors.cyan, gui = "bold" },
---     b = { fg = colors.fg, bg = colors.gray },
---     c = { fg = colors.fg, bg = colors.bg },
---   },
-
---   visual = {
---     a = { fg = colors.bg, bg = colors.orange, gui = "bold" },
---     b = { fg = colors.fg, bg = colors.gray },
---     c = { fg = colors.fg, bg = colors.bg },
---   },
-
---   replace = {
---     a = { fg = colors.bg, bg = colors.purple, gui = "bold" },
---     b = { fg = colors.fg, bg = colors.gray },
---     c = { fg = colors.fg, bg = colors.bg },
---   },
-
---   command = {
---     a = { fg = colors.bg, bg = colors.blue1, gui = "bold" },
---     b = { fg = colors.fg, bg = colors.gray },
---     c = { fg = colors.fg, bg = colors.bg },
---   },
-
---   inactive = {
---     a = { fg = colors.lightgray, bg = colors.bg },
---     b = { fg = colors.lightgray, bg = colors.bg },
---     c = { fg = colors.lightgray, bg = colors.bg },
---   },
--- }
